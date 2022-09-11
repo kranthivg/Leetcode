a7dbea451d1955a78d841de66187dbcdecc1fa86
Kranthi Swarup <36864622+kranthivg@users.noreply.github.com>
2022-09-11 15:16:29 -0500

    commit1

---
+++ Two_Pointers/LeetCode_167.js
@@ -0,0 +1,14 @@
+// Used 2 pointer approach on the sorted input
+var twoSum = function (numbers, target) {
+  let left = 0,
+    right = numbers.length - 1
+  while (left < right) {
+    if (numbers[left] + numbers[right] > target) {
+      right -= 1
+    } else if (numbers[left] + numbers[right] < target) {
+      left += 1
+    } else {
+      return [left + 1, right + 1]
+    }
+  }
+}
