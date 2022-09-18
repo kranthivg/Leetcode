61b192a3ef96ed392b45f9b21ec96232a0a86237
Kranthi Swarup <kranthivg@gmail.com>
2022-09-18 13:27:29 -0500

    neww commit

---
+++ Binary_Search/Leetcode_1351.js
@@ -0,0 +1,16 @@
+var countNegatives = function (grid) {
+  const m = grid.length
+  const n = grid[0].length
+  let left = 0,
+    right = n - 1,
+    count = 0
+  while (left < m && right >= 0) {
+    if (grid[left][right] < 0) {
+      count += m - left
+      right -= 1
+    } else {
+      left += 1
+    }
+  }
+  return count
+}
